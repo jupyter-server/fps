2beba2e014cc8365986980ea97cd9ca2b541a8ad
Adrien DELSALLE <ad.delsalle@gmail.com>
2021-08-05 18:58:55 +0200

    update utils update setup to use new entrypoints

--- fps/utils.py
+++ fps/utils.py
@@ -1,27 +1,24 @@
 import inspect
-import sys
-
-if sys.version_info >= (3, 8):
-    from importlib import metadata as importlib_metadata
-else:
-    import importlib_metadata
+from types import ModuleType
+from typing import Union
 
 
-def get_caller_module_name(stack_level: int = 1):
+def get_caller_module_name(stack_level: int = 1) -> str:
 
     frm = inspect.stack()[stack_level]
     mod = inspect.getmodule(frm[0])
     return mod.__name__
 
 
-def get_pluggin_name(module: str):
-    for dist in list(importlib_metadata.distributions()):
-        for ep in dist.entry_points:
-            if ep.group == "fps" and ep.value == module:
-                return ep.name
+def get_plugin_name(module: Union[str, ModuleType], strip_fps: bool = True) -> str:
+    if not isinstance(module, str):
+        module = module.__name__
 
-    return "default"
+    if module.startswith(("fps-", "fps_")) and strip_fps:
+        module = module[4:]
+
+    return module.split(".")[0]
 
 
-def get_caller_pluggin_name(stack_level: int = 1):
-    return get_pluggin_name(get_caller_module_name(stack_level + 1))
+def get_caller_plugin_name(stack_level: int = 1) -> str:
+    return get_plugin_name(get_caller_module_name(stack_level + 1))
