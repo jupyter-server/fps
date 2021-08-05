7c64de1bcc301378c099b331482d102656c48e7b
Adrien DELSALLE <ad.delsalle@gmail.com>
2021-08-05 18:56:51 +0200

    remove unecessary code router tags are dynamically added using plugin name pluggy hookimpl are exposed using helpers

--- fps/__init__.py
+++ fps/__init__.py
@@ -1,9 +1,1 @@
 from fps._version import __version__  # noqa
-from fps.api import APIRouter  # noqa
-
-try:
-    import pluggy
-
-    hookimpl = pluggy.HookimplMarker("fps")
-except ImportError:
-    pass
