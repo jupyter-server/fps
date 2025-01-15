fea5d1c9f4fba83695e7923297f833ce9b3c52d8
David Brochart <david.brochart@gmail.com>
2025-01-15 14:04:56 +0100

    Add CLI

--- src/fastaio/__init__.py
+++ src/fastaio/__init__.py
@@ -1,3 +1,5 @@
+from ._cli import get_config as get_config
+from ._cli import main as main
 from ._context import Context as Context
 from ._component import Component as Component
 from ._component import initialize as initialize
---
+++ tests/test_cli.py
@@ -0,0 +1,53 @@
+from click.testing import CliRunner
+from fastaio import Component, get_config, main
+
+
+def test_wrong_cli():
+    runner = CliRunner()
+    runner.invoke(main, [
+        "fastaio.Component",
+        "--set", "param",
+        ])
+
+def test_cli():
+    runner = CliRunner()
+    runner.invoke(main, [
+        "fastaio.Component",
+        "--set", "param=-1",
+        "--set", "component0.param0=foo",
+        "--set", "component1.param1=bar",
+        "--set", "component2.param2=baz",
+        "--set", "component2.component3.param3=123",
+        ])
+    assert get_config() == {
+        "root_component": {
+            "type": Component,
+            "config": {
+                "param": "-1"
+            },
+            "components": {
+                "component0": {
+                    "config": {
+                        "param0": "foo"
+                    }
+                },
+                "component1": {
+                    "config": {
+                        "param1": "bar"
+                    }
+                },
+                "component2": {
+                    "config": {
+                        "param2": "baz"
+                    },
+                    "components": {
+                        "component3": {
+                            "config": {
+                                "param3": "123"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
