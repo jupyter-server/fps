98d400ce362961db36e27602cbbd7223e4330dce
David Brochart <david.brochart@gmail.com>
2025-02-04 15:35:14 +0100

    Set version as dynamic

--- src/fps/__init__.py
+++ src/fps/__init__.py
@@ -4,3 +4,5 @@ from ._module import Module as Module
 from ._module import initialize as initialize
 from ._config import get_root_module as get_root_module
 from ._config import merge_config as merge_config
+
+__version__ = "0.0.21"
