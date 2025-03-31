99cf128f9cc5f6207a4323eea6331da49c307ce6
David Brochart <david.brochart@gmail.com>
2025-03-31 21:49:15 +0200

    Add Signal iterator

--- src/fps/_signal.py
+++ src/fps/_signal.py
@@ -2,7 +2,8 @@ from collections.abc import Callable
 from inspect import iscoroutinefunction
 from typing import Generic, TypeVar
 
-from anyio import create_task_group
+from anyio import BrokenResourceError, create_memory_object_stream, create_task_group
+from anyio.streams.memory import MemoryObjectSendStream, MemoryObjectReceiveStream
 
 
 T = TypeVar("T")
@@ -11,6 +12,12 @@ T = TypeVar("T")
 class Signal(Generic[T]):
     def __init__(self) -> None:
         self._callbacks: set[Callable[[T], None]] = set()
+        self._send_streams: set[MemoryObjectSendStream[T]] = set()
+
+    def iterate(self) -> MemoryObjectReceiveStream[T]:
+        send_stream, receive_stream = create_memory_object_stream[T]()
+        self._send_streams.add(send_stream)
+        return receive_stream
 
     def connect(self, callback: Callable[[T], None]) -> None:
         self._callbacks.add(callback)
@@ -19,9 +26,28 @@ class Signal(Generic[T]):
         self._callbacks.remove(callback)
 
     async def emit(self, value: T) -> None:
+        to_remove: list[MemoryObjectSendStream[T]] = []
+
         async with create_task_group() as tg:
             for callback in self._callbacks:
                 if iscoroutinefunction(callback):
                     tg.start_soon(callback, value)
                 else:
                     callback(value)
+
+            for send_stream in self._send_streams:
+                tg.start_soon(self._send, send_stream, value, to_remove)
+
+        for send_stream in to_remove:
+            self._send_streams.remove(send_stream)
+
+    async def _send(
+        self,
+        send_stream: MemoryObjectSendStream[T],
+        value: T,
+        to_remove: list[MemoryObjectSendStream[T]],
+    ) -> None:
+        try:
+            await send_stream.send(value)
+        except BrokenResourceError:
+            to_remove.append(send_stream)
--- tests/test_signal.py
+++ tests/test_signal.py
@@ -1,30 +1,64 @@
 import pytest
 
+from anyio import TASK_STATUS_IGNORED, create_task_group
+from anyio.abc import TaskStatus
 from fps import Signal
 
 pytestmark = pytest.mark.anyio
 
 
-async def test_signal():
+async def test_signal_callback():
     signal = Signal()
-    values = []
+    values0 = []
+    values1 = []
 
     async def acallback(value):
-        values.append(f"a{value}")
+        values0.append(f"a{value}")
+
+    def callback(value):
+        values1.append(value)
 
     signal.connect(acallback)
+    signal.connect(callback)
 
     await signal.emit("foo")
     await signal.emit("bar")
 
-    assert values == ["afoo", "abar"]
+    assert values0 == ["afoo", "abar"]
+    assert values1 == ["foo", "bar"]
 
     signal.disconnect(acallback)
 
-    def callback(value):
-        values.append(value)
-
-    signal.connect(callback)
     await signal.emit("baz")
 
-    assert values == ["afoo", "abar", "baz"]
+    assert values0 == ["afoo", "abar"]
+    assert values1 == ["foo", "bar", "baz"]
+
+
+async def test_signal_iterator():
+    signal = Signal()
+    values0 = []
+    values1 = []
+
+    async def task(values, idx, *, task_status: TaskStatus[None] = TASK_STATUS_IGNORED):
+        async with signal.iterate() as iterator:
+            task_status.started()
+            async for value in iterator:
+                values.append(f"{value}{idx}")
+                if idx == 0:
+                    if value == "bar":
+                        return
+                else:
+                    if value == "baz":
+                        return
+
+    async with create_task_group() as tg:
+        await tg.start(task, values0, 0)
+        await tg.start(task, values1, 1)
+
+        await signal.emit("foo")
+        await signal.emit("bar")
+        await signal.emit("baz")
+
+    assert values0 == ["foo0", "bar0"]
+    assert values1 == ["foo1", "bar1", "baz1"]
