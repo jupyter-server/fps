1705a040a179a3a97dab3a9a4abc5a581c8d0b2a
David Brochart <david.brochart@gmail.com>
2025-05-22 17:49:40 +0200

    Extract out server from FastAPIModule to ServerModule

--- src/fps/web/fastapi.py
+++ src/fps/web/fastapi.py
@@ -1,10 +1,5 @@
 from __future__ import annotations
 
-from functools import partial
-
-from anyio import Event, connect_tcp, create_task_group
-from anyioutils import start_task
-from anycorn import Config, serve
 from fastapi import FastAPI
 
 from fps import Module
@@ -16,44 +11,11 @@ class FastAPIModule(Module):
         name: str,
         *,
         app: FastAPI | None = None,
-        host: str = "127.0.0.1",
-        port: int = 8000,
         debug: bool | None = None,
     ) -> None:
         super().__init__(name)
         debug = debug if debug is not None else __debug__
         self.app = app if app is not None else FastAPI(debug=debug)
-        self.host = host
-        self.port = port
-        self.shutdown_event = Event()
 
     async def prepare(self) -> None:
         self.put(self.app)
-
-    async def start(self) -> None:
-        config = Config()
-        config.bind = [f"{self.host}:{self.port}"]
-        config.loglevel = "WARN"
-        async with create_task_group() as tg:
-            self.server_task = start_task(
-                partial(
-                    serve,
-                    self.app,  # type: ignore[arg-type]
-                    config,
-                    shutdown_trigger=self.shutdown_event.wait,
-                    mode="asgi",
-                ),
-                tg,
-            )
-            while True:
-                try:
-                    await connect_tcp(self.host, self.port)
-                except OSError:
-                    pass
-                else:
-                    break
-            self.done()
-
-    async def stop(self) -> None:
-        self.shutdown_event.set()
-        await self.server_task.wait()
